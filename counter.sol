ec8a327f7c741c19cf9a2d7e7135390df8848960
Rexford Machu <rexfordmachu@gmail.com>
2021-11-10 16:11:00 +0000

    First solidity contract

---
+++ counter.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.8.10;
+
+contract Counter {
+    
+    // Public variable of type unsigned int to keep the number of counts
+    uint256 public count = 0;
+
+    // Function that increments our counter
+    function increment() public {
+        count += 1;
+    }
+    
+    // Not necessary getter to get the count value
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+}
+
